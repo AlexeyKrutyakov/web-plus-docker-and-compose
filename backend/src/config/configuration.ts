0609d169b843893ef7d4ca7cb60386ff42d548fd
Alexey Krutyakov <coder.arctik@gmail.com>
2024-05-16 15:37:22 +0300

    fix: change constants names for db in backend config

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -4,11 +4,11 @@ export default () => ({
     port: parseInt(process.env.SERVER_PORT) || '3000',
   },
   database: {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || 'student',
-    password: process.env.DB_PASSWORD || 'student',
-    name: process.env.DB_NAME || 'kupipodariday',
+    host: process.env.POSTGRES_HOST || 'postgres',
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    user: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || 'student',
+    name: process.env.POSTGRES_NAME || 'kupipodariday',
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'very-secret',
